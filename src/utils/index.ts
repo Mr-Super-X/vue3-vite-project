79e2696194a37c9b12d4830c3e11b33d02be1b86
Mr.Mickey <1303232158@qq.com>
2026-07-21 17:33:39 +0800

    feat(utils): 添加控制台badge徽章工具函数
    新增showBadge函数用于在控制台输出类似github README中的badge徽章，
    支持自定义标签、值和背景颜色，并在utils模块中导出该功能。
    
    函数参数包括label(标签)、value(标签内容)、bgColor1(背景颜色1)、
    bgColor2(背景颜色2)，可通过CSS样式实现彩色徽章效果。

---
+++ src/utils/index.ts
@@ -0,0 +1,4 @@
+export * from './bem' // 导出 bem 模块所有的配置
+export * from './dayjs' // 导出 dayjs 模块所有的配置
+export * from './storage' // 导出storage模块所有的配置
+export * from './consoleBadge' // 导出 consoleBadge 模块所有的配置
