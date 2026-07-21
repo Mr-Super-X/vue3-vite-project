69b58953e740e8e2e7d2e144abd447a4f3da3175
Mr.Mickey <1303232158@qq.com>
2026-07-21 17:54:12 +0800

    docs(utils): 添加utils模块统一导出说明
    在 src/utils/index.ts 中统一导出 utils 下的所有工具函数，方便在其他地方直接使用
    
    例如：
    import { Local, Session, clearByNamespace } from '@/utils'
    Local.set('token', 'xxx')
    Session.get('userInfo')

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,12 @@
+/**
+ * 在 src/utils/index.ts 中统一导出 utils 下的所有工具函数，方便在其他地方直接使用
+ *
+ * 例如：
+ * import { Local, Session, clearByNamespace } from '@/utils'
+ * Local.set('token', 'xxx')
+ * Session.get('userInfo')
+ */
+
 export * from './bem' // 导出 bem 模块所有的配置
 export * from './dayjs' // 导出 dayjs 模块所有的配置
 export * from './storage' // 导出storage模块所有的配置
