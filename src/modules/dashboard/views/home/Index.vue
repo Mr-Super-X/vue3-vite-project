d51df50c8fb3e7d4a72e3558ec77cfbf723db3c2
Mr.Mickey <1303232158@qq.com>
2026-07-23 18:12:49 +0800

    feat(portal-home): home/Index.vue 组合页面

---
+++ src/modules/dashboard/views/home/Index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { usePortalOverviewStore } from '@/modules/dashboard/store/portal-overview'
+import HeroSection from './components/HeroSection.vue'
+import DateGreeting from './components/DateGreeting.vue'
+import OverviewSection from './components/OverviewSection.vue'
+
+const store = usePortalOverviewStore()
+
+onMounted(() => {
+  store.fetch()
+})
+</script>
+
+<template>
+  <div class="home">
+    <HeroSection />
+    <DateGreeting />
+    <OverviewSection />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+}
+</style>
