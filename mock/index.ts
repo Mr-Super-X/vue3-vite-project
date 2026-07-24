623d426d8323afb9c0a9ad2e6f78aba49c2d0524
Mr.Mickey <1303232158@qq.com>
2026-07-24 09:28:18 +0800

    refactor(mock): 删除 dashboard 死代码 mock
    dashboard 模块重命名为 home 后，/api/dashboard/stats 接口全项目无引用。
    - git rm mock/dashboard.ts
    - mock/index.ts 同步移除 import 与 ...dashboard 拼接项

--- mock/index.ts
+++ mock/index.ts
@@ -1,6 +1,5 @@
 import auth from './auth'
 import user from './user'
-import dashboard from './dashboard'
 import menu from './menu'
 
-export default [...auth, ...user, ...dashboard, ...menu]
+export default [...auth, ...user, ...menu]
