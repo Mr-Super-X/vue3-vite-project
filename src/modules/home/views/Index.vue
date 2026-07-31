0cfa571d1d787fd7720a5eed093af629e4eea56e
Mr.Mickey <1303232158@qq.com>
2026-07-31 16:25:23 +0800

    refactor(home): 组件目录结构调整
    将home模块中的组件从views/components目录迁移至components目录，
    统一组件存放位置以便更好地组织代码结构。
    
    BREAKING CHANGE: 组件路径发生变化，相关引用需要更新。

--- src/modules/home/views/Index.vue
+++ src/modules/home/views/Index.vue
@@ -3,11 +3,11 @@
 // PortalHeader 在父级 layouts/portal 内；导航以下（含本页）全部由 home 模块自管
 import { onMounted } from 'vue'
 import { usePortalOverviewStore } from '@/modules/home/store/portal-overview'
-import HeroSection from './components/HeroSection.vue'
-import DateGreeting from './components/DateGreeting.vue'
-import OverviewSection from './components/OverviewSection.vue'
-import HomeFooter from './components/HomeFooter.vue'
-import HomeAiWidget from './components/HomeAiWidget.vue'
+import HeroSection from '../components/HeroSection.vue'
+import DateGreeting from '../components/DateGreeting.vue'
+import OverviewSection from '../components/OverviewSection.vue'
+import HomeFooter from '../components/HomeFooter.vue'
+import HomeAiWidget from '../components/HomeAiWidget.vue'
 
 const store = usePortalOverviewStore()
 
